dd5540d3f063a693497be2c7b95cb571e8874676
Alexander Mills <alex@oresoftware.com>
2026-09-15 14:16:36 -0500

    fix(zpkg): make artifact smoke self-contained

--- scripts/smoke-published-artifact.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-set -eu
-
-target="${ZED_PKG_TEST_TARGET:?ZED_PKG_TEST_TARGET is required}"
-
-cargo test --manifest-path "$target/Cargo.toml" --all-targets --locked
-test -x "$target/target/release/zed-sidecar"
